9e53df3558c6e4acb4d6beb01477039298d5c059
Umidjon <umidjoasdasdas@gmail.com>
2024-08-20 10:35:50 +0500

    Resolved merge conflicts

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://backend-web-virid.vercel.app",
 });
 
 export const googleAuth = (code) => api.get(`/auth/google?code=${code}`);
